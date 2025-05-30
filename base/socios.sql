0b939e8c1b46bbfb742e42f473450a2499499c62
anthony <anthony3008@gmail.com>
2025-05-30 13:53:38 +0200

    nuevo datos

--- base/socios.sql
+++ base/socios.sql
@@ -6,10 +6,12 @@ CREATE TABLE socios (
     id_socios INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(80) NOT NULL,
+    direccion VARCHAR(100) NOT NULL,
     telefono VARCHAR(10) NOT NULL,
     email VARCHAR(60) NOT NULL,
-    direccion VARCHAR(100) NOT NULL,
     usuario VARCHAR(50) NOT NULL,
-    clave Varbinary(50) NOT NULL,
-    fecha_alta DATETIME NOT NULL
+    clave Varbinary(100) NOT NULL,
+    fecha_alta DATE NOT NULL
 )
+
+drop table socios;
